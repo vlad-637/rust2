e83e47bb9693a4e4e65674f281a841fd2b32089f
Vlad <vlad.khvesyukovich@gmail.com>
2024-05-18 16:27:06 +0300

    todos/:id update and delete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap, sync::{Arc, RwLock}, time::Duration};
-use axum::{error_handling::HandleErrorLayer, extract::State, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use axum::{error_handling::HandleErrorLayer, extract::{Path, State}, http::StatusCode, response::IntoResponse, routing::{get, patch}, Json, Router};
 use serde::{Deserialize, Serialize};
 use tower::{BoxError, ServiceBuilder};
 use uuid::Uuid;
@@ -10,6 +10,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/todos", get(todos_index).post(todos_create))
+        .route("/todos/:id", patch(todos_update).delete(todos_delete))
         .layer(
             ServiceBuilder::new()
                 .layer(HandleErrorLayer::new(|error: BoxError| async move {
@@ -61,6 +62,45 @@ async fn todos_create(State(db): State<Db>, Json(input): Json<CreateTodo>) -> im
     db.write().unwrap().insert(todo.id, todo.clone());
 
     (StatusCode::CREATED, Json(todo))
+}
+
+#[derive(Debug, Deserialize)]
+struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+async fn todos_update(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+    Json(input): Json<UpdateTodo>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let mut todo = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    if let Some(text) = input.text {
+        todo.text = text;
+    }
+
+    if let Some(completed) = input.completed {
+        todo.completed = completed;
+    }
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    Ok(Json(todo))
+}
+
+async fn todos_delete(Path(id): Path<Uuid>, State(db): State<Db>) -> impl IntoResponse {
+    if db.write().unwrap().remove(&id).is_some() {
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
+    }
 }
 
 type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
